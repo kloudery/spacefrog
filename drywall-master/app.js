74253024e944b80215282686bd37a5c436d372fb
steve <tiendo0711@gmail.com>
2016-07-24 21:27:42 +0700

    Add sign up function after user launch app

--- drywall-master/app.js
+++ drywall-master/app.js
@@ -1,107 +1,101 @@
-'use strict';
+'use strict'
 
-//dependencies
+// dependencies
 var config = require('./config'),
-    express = require('express'),
-    cookieParser = require('cookie-parser'),
-    bodyParser = require('body-parser'),
-    session = require('express-session'),
-    mongoStore = require('connect-mongo')(session),
-    http = require('http'),
-    path = require('path'),
-    passport = require('passport'),
-    mongoose = require('mongoose'),
-    helmet = require('helmet'),
-    csrf = require('csurf');
+  express = require('express'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  session = require('express-session'),
+  mongoStore = require('connect-mongo')(session),
+  http = require('http'),
+  path = require('path'),
+  passport = require('passport'),
+  mongoose = require('mongoose'),
+  helmet = require('helmet'),
+  csrf = require('csurf')
 
-var api = require('./api.js');
+var api = require('./api.js')
 
-//create express app
-var app = express();
+// create express app
+var app = express()
 
-//keep reference to config
-app.config = config;
-console.log("booting");
-//setup the web server
-app.server = http.createServer(app);
+// keep reference to config
+app.config = config
+console.log('booting')
+// setup the web server
+app.server = http.createServer(app)
 
-//setup mongoose
-app.db = mongoose.createConnection(config.mongodb.uri);
-app.db.on('error', console.error.bind(console, 'mongoose connection error: '));
+// setup mongoose
+app.db = mongoose.createConnection(config.mongodb.uri)
+app.db.on('error', console.error.bind(console, 'mongoose connection error: '))
 app.db.once('open', function () {
-  //Build out key items if needed
+  // Build out key items if needed
 
-});
+})
 
-//config data models
-require('./models')(app, mongoose);
+// config data models
+require('./models')(app, mongoose)
 
-//settings
-app.disable('x-powered-by');
-app.set('port', config.port);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// settings
+app.disable('x-powered-by')
+app.set('port', config.port)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
-
-
-//middleware
-app.use(require('morgan')('dev'));
-app.use(require('compression')());
-app.use(require('serve-static')(path.join(__dirname, 'public')));
-app.use(require('method-override')());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser(config.cryptoKey));
+// middleware
+app.use(require('morgan')('dev'))
+app.use(require('compression')())
+app.use(require('serve-static')(path.join(__dirname, 'public')))
+app.use(require('method-override')())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser(config.cryptoKey))
 app.use(session({
   resave: true,
   saveUninitialized: true,
   secret: config.cryptoKey,
   store: new mongoStore({ url: config.mongodb.uri })
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(csrf({ cookie: { signed: true } }));
-helmet(app);
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(csrf({ cookie: { signed: true } }))
+helmet(app)
 
-//response locals
-app.use(function(req, res, next) {
-  res.cookie('_csrfToken', req.csrfToken());
-  res.locals.user = {};
-  res.locals.user.defaultReturnUrl = req.user && req.user.defaultReturnUrl();
-  res.locals.user.username = req.user && req.user.username;
-  next();
-});
+// response locals
+app.use(function (req, res, next) {
+  res.cookie('_csrfToken', req.csrfToken())
+  res.locals.user = {}
+  res.locals.user.defaultReturnUrl = req.user && req.user.defaultReturnUrl()
+  res.locals.user.username = req.user && req.user.username
+  next()
+})
 
-//global locals
-app.locals.projectName = app.config.projectName;
-app.locals.copyrightYear = new Date().getFullYear();
-app.locals.copyrightName = app.config.companyName;
-app.locals.cacheBreaker = 'br34k-01';
+// global locals
+app.locals.projectName = app.config.projectName
+app.locals.copyrightYear = new Date().getFullYear()
+app.locals.copyrightName = app.config.companyName
+app.locals.cacheBreaker = 'br34k-01'
 
-//setup passport
-require('./passport')(app, passport);
-
+// setup passport
+require('./passport')(app, passport)
 
 // AJAX API, no login needed.  If we need login/userdata need to move inside routes.
-app.use('/api', require('./api.js'));
+app.use('/api', require('./api.js'))
 
+// setup routes
+require('./routes')(app, passport)
 
-//setup routes
-require('./routes')(app, passport);
+// custom (friendly) error handler
+app.use(require('./views/http/index').http500)
 
-//custom (friendly) error handler
-app.use(require('./views/http/index').http500);
+// setup utilities
+app.utility = {}
+app.utility.sendmail = require('./util/sendmail')
+app.utility.slugify = require('./util/slugify')
+app.utility.workflow = require('./util/workflow')
 
-
-
-//setup utilities
-app.utility = {};
-app.utility.sendmail = require('./util/sendmail');
-app.utility.slugify = require('./util/slugify');
-app.utility.workflow = require('./util/workflow');
-
-//listen up
-app.server.listen(app.config.port, function(){
-  //and... we're live
-  console.log('Server is running on port ' + config.port);
-});
+// listen up
+app.server.listen(app.config.port, function () {
+  // and... we're live
+  console.log('Server is running on port ' + config.port)
+})
