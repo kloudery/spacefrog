2f9d45930751578dd37ee235b69d300216f08667
steve <tiendo0711@gmail.com>
2016-07-26 00:06:18 +0700

    Complete sign up function with Stripe card info checking after user launch an app

---
+++ drywall-master/public/views/signup/indexwithstripe.js
@@ -0,0 +1,92 @@
+/* global app:true */
+
+(function() {
+  'use strict';
+
+  app = app || {};
+
+  app.Signup = Backbone.Model.extend({
+    url: '/signup/',
+    defaults: {
+      errors: [],
+      errfor: {},
+      username: '',
+      email: '',
+      password: ''
+    }
+  });
+
+  app.SignupView = Backbone.View.extend({
+    el: '#signup',
+    template: _.template( $('#tmpl-signup').html() ),
+    events: {
+      'submit form': 'createStripeToken',
+      //'keypress [name="password"]': 'signupOnEnter',
+      //'click .btn-signup': 'signup'
+    },
+    initialize: function() {
+      this.model = new app.Signup();
+      this.listenTo(this.model, 'sync', this.render);
+      this.render();
+    },
+    render: function() {
+      this.$el.html(this.template( this.model.attributes ));
+      this.$el.find('[name="username"]').focus();
+    },
+    createStripeToken: function(event) {
+      event.preventDefault();
+      Stripe.setPublishableKey('pk_test_4MT0GF18tCIve3NiwrXEhIvK');
+      var $form = this.$('#payment-form');
+      // Disable the submit button to prevent repeated clicks
+      $form.find('button').prop('disabled', true);
+      Stripe.createToken($form, this.stripeResponseHandler);
+      // Prevent the form from submitting with the default action
+    },  // createStripeToken
+
+    stripeResponseHandler: function(status,response) {
+      var $form = $('#payment-form');
+      if (response.error) {
+        // Show the errors on the form
+        $form.find('.payment-errors').text(response.error.message);
+        $form.find('button').prop('disabled', false);
+      } else {
+        // token contains id, last4, and card type
+        var token = response.id;
+        // Insert the token into the form so it gets submitted to the server
+        $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+        // and submit
+        this.signup();
+      }
+    },
+
+    //signupOnEnter: function(event) {
+    //  if (event.keyCode !== 13) { return; }
+    //  if ($(event.target).attr('name') !== 'password') { return; }
+    //  event.preventDefault();
+    //  this.signup();
+    //},
+    signup: function() {
+      this.$el.find('.btn-signup').attr('disabled', true);
+
+      this.model.save({
+        username: this.$el.find('[name="username"]').val(),
+        email: this.$el.find('[name="email"]').val(),
+        password: this.$el.find('[name="password"]').val(),
+        stripeToken: this.$el.find('[name="stripeToken"]').val()
+      },{
+        success: function(model, response) {
+          if (response.success) {
+            location.href = '/account/';
+          }
+          else {
+            model.set(response);
+          }
+        }
+      });
+    }
+  });
+
+  $(document).ready(function() {
+    app.signupView = new app.SignupView();
+  });
+}());
