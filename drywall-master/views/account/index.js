0d95f8be5b9f1bcb11f9b6be5887a6ff6223b044
Jacob Gadikian <faddat@gmail.com>
2016-08-04 02:19:31 +0700

    moved a great many things.

--- drywall-master/views/account/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var apps = require('../../config/apps.json');
-
-exports.init = function(req, res){
-  res.render('account/index', {apps: apps});
-};
--- drywall-master/views/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-var apps = require('../config/apps.json');
-
-exports.init = function (req, res) {
-  res.render('index', {apps: apps});
-};
